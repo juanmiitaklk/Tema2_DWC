50455c9d28dbcdeebe244f9f9c7abe9dee5e19aa
juanmiitaklk <143542276+juanmiitaklk@users.noreply.github.com>
2024-11-06 19:22:13 +0100

    First Commit

---
+++ Ejercicio1/ejercicio1.js
@@ -0,0 +1,11 @@
+const cuerpoTabla = document.getElementById("cuerpo-tabla");
+
+const producto = document.getElementById("producto");
+
+const cantidad = document.getElementById("cantidad");
+
+const precioUnitario = document.getElementById("precio-unitario");
+
+const descuento = document.getElementById("descuento");
+
+const agregarLinea = document.getElementById("agregarLinea");
---
+++ Ejercicio2/Ejercicio2.js
@@ -0,0 +1,18 @@
+// Función que recoge los valores de los inputs y los muestra en la consola
+function mostrarValores() {
+  const producto = document.getElementById("producto").value;
+  const cantidad = document.getElementById("cantidad").value;
+  const precioUnitario = document.getElementById("precio-unitario").value;
+  const descuento = document.getElementById("descuento").value;
+
+  console.log("Producto:", producto);
+  console.log("Cantidad:", cantidad);
+  console.log("Precio unitario:", precioUnitario);
+  console.log("Descuento:", descuento);
+}
+
+// Asignar la función mostrarValores al evento click del botón "Agregar"
+document.getElementById("agregarLinea").addEventListener("click", function(event) {
+  event.preventDefault();  // Evitar que el enlace recargue la página
+  mostrarValores();
+});
